3eaa5d7e2b7455e14c2f28e5923260285164857d
NamikR <uchitelnamik@mail.ru>
2022-10-01 13:28:25 +0400

    Pervaya zadacha reshena

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿// // Урок 1. Знакомство с языком программирования С#
+// // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// // a = 5; b = 7 -> max = 7
+// // a = 2 b = 10 -> max = 10
+// // a = -9 b = -3 -> max = -3
+
+// int pervoyeChislo = 0; // Obyavlayem peremenniye 
+// int vtoroyeChislo = 0; 
+
+// Console.Write("Задача 2: \n Напишите программу, которая на вход принимает два числа \n и выдаёт, какое число большее, а какое меньшее.");
+
+// Console.Write("\n"); // "для создание оступов между строками"
+// Console.Write("\n");
+
+// Console.Write("Решение");
+
+// Console.Write("\n");// "для создание оступов между строками"
+// Console.Write("\n");
+
+// Console.Write("Задайте первое число ");
+// pervoyeChislo = Convert.ToInt32(Console.ReadLine()); // "Принимаем данные от пользователя"
+// Console.Write("Задайте второе число ");
+// vtoroyeChislo = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("\n");
+
+//      if(pervoyeChislo > vtoroyeChislo) // "начало условия"
+//          {
+//              Console.Write("Из заданных данных " + pervoyeChislo  + " являеется большим числом а "+ vtoroyeChislo + " меньшим числом  ");
+//          }
+//          else
+//          {
+//              Console.Write("Из заданных данных " + vtoroyeChislo  + " являеется большим числом а " + pervoyeChislo  + " меньшим числом  ");
+//          }   // "конец условия"
+
+// // Первая задача завершена. ------------------------------------------------------------------------
+
+
+
+// int a =0;
+// string? name = "";
+
+// Console.Write("Введите свое имя: ");
+
+// name = Console.ReadLine();
+// a =  5 + Convert.ToInt32(name);
+
+// Console.WriteLine($"Привет {a}");
