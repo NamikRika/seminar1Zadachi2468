d072725dfcb0ed573b85dccb423ceb6a10b89367
NamikR <uchitelnamik@mail.ru>
2022-10-01 14:01:20 +0400

    Vtoraya zadacha reshena

--- Program.cs
+++ Program.cs
@@ -33,7 +33,56 @@
 //              Console.Write("Из заданных данных " + vtoroyeChislo  + " являеется большим числом а " + pervoyeChislo  + " меньшим числом  ");
 //          }   // "конец условия"
 
-// // Первая задача завершена. ------------------------------------------------------------------------
+// // задача 2 завершена. ------------------------------------------------------------------------
+
+
+
+// // Начало задачи 4
+
+// // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// // 2, 3, 7 -> 7
+// // 44 5 78 -> 78
+// // 22 3 9 -> 22
+
+// int pervoyeChislo = 0; // Obyavlayem peremenniye 
+// int vtoroyeChislo = 0; 
+// int tretyeChislo = 0; 
+
+// Console.Write("Задача 4: \n Напишите программу, которая на вход принимает три числа \n и выдаёт, максимальное из этих чисел.");
+
+// Console.Write("\n"); // "для создание оступов между строками"
+// Console.Write("\n");
+
+// Console.Write("Решение");
+
+// Console.Write("\n");// "для создание оступов между строками"
+// Console.Write("\n");
+
+// Console.Write("Задайте первое число ");
+// pervoyeChislo = Convert.ToInt32(Console.ReadLine()); // "Принимаем данные от пользователя"
+// Console.Write("Задайте второе число ");
+// vtoroyeChislo = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Задайте третье число ");
+// tretyeChislo = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("\n");
+
+//      if (pervoyeChislo > vtoroyeChislo && pervoyeChislo > tretyeChislo) // "начало условия"
+//            {
+//                Console.Write("Из чисел " + pervoyeChislo  + ", " + vtoroyeChislo + ", " +  tretyeChislo + ", " + " максимальным является " +  pervoyeChislo);
+//            }
+//          else if (vtoroyeChislo > pervoyeChislo && vtoroyeChislo > tretyeChislo)
+//            {
+//                 Console.Write("Из чисел " + pervoyeChislo  + ", " + vtoroyeChislo + ", " +  tretyeChislo + ", " + " максимальным является " + vtoroyeChislo);
+//            }   // "конец условия"
+//          else if (tretyeChislo > pervoyeChislo && tretyeChislo > vtoroyeChislo)
+//            {
+//                 Console.Write("Из чисел " + pervoyeChislo  + ", " + vtoroyeChislo + ", " +  tretyeChislo + ", " + " максимальным является " + tretyeChislo);
+//            }   // "конец условия"
+
+// // задача 4 завершена. ------------------------------------------------------------------------
+
+// Начало задачи 6
 
 
 
